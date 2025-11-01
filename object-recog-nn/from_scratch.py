c2e1c001413cb163911a5e610947dee7c597da54
arinagn <arina.ghanizad@gmail.com>
2025-11-01 14:11:50 +0300

    Ran ruff formatter on from_scratch module.

--- object-recog-nn/from_scratch.py
+++ object-recog-nn/from_scratch.py
@@ -10,22 +10,23 @@ def initialize_parameters_l_layers(layer_dims):
 
     Args:
         layer_dims: An array containing the dimensions of each layer in our network.
-                    e.g. (n_x, n_h, n_y) where n_x denotes the number of units in the 
-                    input layer (l=0), n_h is the number of units in the hidden layer 
-                    (l=1) and n_y is the number of units in the output layer of this 
+                    e.g. (n_x, n_h, n_y) where n_x denotes the number of units in the
+                    input layer (l=0), n_h is the number of units in the hidden layer
+                    (l=1) and n_y is the number of units in the output layer of this
                     2 layer neural network.
-    
+
     Returns:
         parameters: A dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
                     Wl - weight matrix of shape (layer_dims[l], layer_dims[l-1])
                     bl - bias vector of shape (layer_dims[l], 1)
     """
     parameters = {}
-    L = len(layer_dims) # number of layers in the network
+    L = len(layer_dims)  # number of layers in the network
 
-    for l in range(1, L): # noqa: E741
-        parameters["W" + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * 0.01
+    for l in range(1, L):  # noqa: E741
+        parameters["W" + str(l)] = (
+            np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
+        )
         parameters["b" + str(l)] = np.zeros((layer_dims[l], 1))
 
     return parameters
-
