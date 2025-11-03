dc60d2782db314cdaad984cc55d9f41335e9135c
arinagn <arina.ghanizad@gmail.com>
2025-11-03 14:09:49 +0300

    Added forward prop method.

--- object-recog-nn/with_pytorch.py
+++ object-recog-nn/with_pytorch.py
@@ -26,3 +26,15 @@ class LLayerNN(nn.Module):
 
         # Define Sequential model where all layers stacked
         self.network = nn.Sequential(*layers)
+
+    def forward(self, X):
+        """Performs forward prop, linear -> activation, across L layers.
+
+        Args:
+            X: Input feature matrix of shape (m, n_x). If only one training example
+               is used (m=1, non-vectorised), this is an input feature vector.
+               Note that PyTorch prefers one row per training example instead of
+               the notation used in ML mathematics  where rows represent a feature
+               vector for all m training examples.
+        """
+        return self.network(X)
